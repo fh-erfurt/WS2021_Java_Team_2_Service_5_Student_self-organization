7e7cfd4f644184396b5d9048f15b851ba7ef4431
fr0321ta <Friedemann.Taubert@fh-erfurt.de>
2022-01-26 16:50:37 +0100

    added List and ListElement

---
+++ src/main/java/de/fherfurt/organization/ListElement.java
@@ -0,0 +1,28 @@
+package de.fherfurt.organization;
+
+// defines how the elements of the list are built up
+
+public class ListElement
+{
+    String s;
+    ListElement next;
+
+    public ListElement(String s)
+    {
+        this.s = s;
+        next = null;    // in case it's the last element
+    }
+
+
+    public boolean hasNext()
+    {
+        if (this.next == null)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+}
