d9c021bf1a094f739575d459d03796ed7fd951bb
fr0321ta <Friedemann.Taubert@fh-erfurt.de>
2022-03-17 14:22:02 +0100

    Forum Rework überarbeitet

---
+++ src/main/java/de/fherfurt/organization/forum/Forum.java
@@ -0,0 +1,36 @@
+package de.fherfurt.organization.forum;
+
+import java.util.ArrayList;
+
+public class Forum {
+    private ArrayList<String> topics;
+    private ArrayList<Question> questions;
+
+    public void addNewQuestion(){
+
+    }
+
+    public void removeQuestion(String title){
+
+    }
+
+    public ArrayList<Question> getByTopic (String topic){
+
+    }
+
+    public ArrayList<Question> getByQuestionTitle (String question){
+
+    }
+
+    public ArrayList<Question> getByQuestionAuthor (String author){
+
+    }
+
+    public boolean addNewTopic (String newTopic){
+
+    }
+
+    public boolean removeTopic (String topic){
+
+    }
+}
